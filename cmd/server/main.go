ff1ceb3a3cf3006d511de768d6c5f49094a1da8e
Tamer_Li <tamer@li-work08.ru>
2026-06-29 21:51:11 +0300

    add handler main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+var gauge float64
+
+type MemStorage struct {
+	counters []int64
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Main Page"))
+}
+
+func gaugeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		rData := r.URL.Path
+		fmt.Println(rData)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/update/gauge`, gaugeHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Tamer-Li/devops-metrics
+
+go 1.25.0
