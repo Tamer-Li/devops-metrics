c8715cf7554ab4411391d5d001fac97f5664a1cb
Tamer_Li <tamer@li-work08.ru>
2026-07-05 23:51:02 +0300

    add chi server

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,8 +3,10 @@ package repository
 type MetricsStorage interface {
 	GaugeSet(name string, value float64)
 	CounterSet(name string, value int64)
-	Gauge() map[string]float64
-	Counter() map[string]int64
+	Gauges() map[string]float64
+	Counters() map[string]int64
+	Gauge(name string) (float64, bool)
+	Counter(name string) (int64, bool)
 }
 
 type RepoMemStats interface {
