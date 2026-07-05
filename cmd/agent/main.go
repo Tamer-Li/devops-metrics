00a9261fb77d735a64fde66566fd3d2a70b151e0
Tamer_Li <tamer@li-work08.ru>
2026-07-06 00:36:08 +0300

    add flag

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/Tamer-Li/devops-metrics/internal/agent"
 	"github.com/Tamer-Li/devops-metrics/internal/service"
 )
 
+var settings struct {
+	address        string
+	reportInterval int
+	pollInterval   int
+}
+
+func init() {
+	flag.StringVar(&settings.address, "a", "localhost:8080", "address endpoint http-server")
+	flag.IntVar(&settings.reportInterval, "r", 10, "report interval in seconds")
+	flag.IntVar(&settings.pollInterval, "p", 2, "poll interval in seconds")
+}
+
 func main() {
-	pollInterval := 2
-	reportInterval := 10
-	url := "http://localhost:8080"
+	flag.Parse()
+	url := fmt.Sprintf("http://%s", settings.address)
 	memStats := service.NewMemStatsRuntime()
 
 	agentMy := agent.NewAgentMetric(
 		url,
-		pollInterval,
-		reportInterval,
+		settings.pollInterval,
+		settings.reportInterval,
 		memStats,
 	)
 
